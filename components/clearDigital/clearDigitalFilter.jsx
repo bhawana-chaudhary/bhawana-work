1cf8f99080b838f498343f7374d2b05d3ee3bbcc
Bhawana <bhawana@webenertia.com>
2024-03-12 19:37:42 +0530

    update

--- components/clearDigital/clearDigitalFilter.jsx
+++ components/clearDigital/clearDigitalFilter.jsx
@@ -63,18 +63,62 @@ const filtercardData = [
 
 export default function ClearDigitalFilter() {
   const [openAccordionIndex, setOpenAccordionIndex] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
+
   const toggleAccordion = (index) => {
     setOpenAccordionIndex((prevIndex) => (prevIndex === index ? null : index));
   };
 
+  // useEffect(() => {
+  //   // Event handler for filtering search
+  //   $("#fSearch").on("keyup", function () {
+  //     var value = $(this).val().toLowerCase();
+  //     $(".filter_card").filter(function () {
+  //       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+  //     });
+  //   });
+  // }, []);
+
   useEffect(() => {
-    $("#fSearch").on("keyup", function () {
-      var value = $(this).val().toLowerCase();
-      $(".filter_card").filter(function () {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    const searchInput = document.getElementById("fSearch");
+    const filterCards = document.querySelectorAll(".filter_card");
+
+    if (searchInput) {
+      searchInput.addEventListener("keyup", function (event) {
+        const value = event.target.value.toLowerCase();
+        filterCards.forEach((card) => {
+          const cardText = card.innerText.toLowerCase();
+          if (cardText.includes(value) || value === "") {
+            card.style.display = "block";
+          } else {
+            card.style.display = "none";
+          }
+        });
+        // filterCards.forEach((card) => {
+        //   const cardText = card.innerText.toLowerCase();
+        //   if (cardText.includes(value) || value === "") {
+        //     card.style.display = "block";
+        //     $(this).closest(".search_icon").hide;
+        //   } else {
+        //     card.style.display = "none";
+        //     $(this).closest(".search_icon").show;
+        //   }
+        // });
       });
-    });
+    }
+
+    return () => {
+      if (searchInput) {
+        searchInput.removeEventListener("keyup");
+      }
+    };
   }, []);
+  const showAllCards = () => {
+    const filterCards = document.querySelectorAll(".filter_card");
+    filterCards.forEach((card) => {
+      card.style.display = "block";
+    });
+  };
 
   return (
     <>
@@ -82,15 +126,38 @@ export default function ClearDigitalFilter() {
         <div className={`${HongoStyle.container_big}`}>
           <div className=" clearDigital_FilterWrap relative w-full flex flex-wrap  ">
             <div className="filter_header relative w-full max-w-[407px] ">
-              <div className="search_wrap relative w-full mb-[30px] after:content-[''] after:absolute after:top-[20px] after:right-[20px] after:w-[25px] after:h-[25px] after:bg-[url('/clearDigital/arrow_search.svg')] after:bg-no-repeat after:bg-contain ">
+              <div className="search_wrap relative w-full mb-[30px] ">
                 <input
                   type="search"
                   id="fSearch"
-                  className=" relative w-full bg-transparent border-white border-[2px] border-solid text-[16px] font-medium font-poppins py-[21px] pl-[15px] pr-[40px] text-white outline-none "
+                  className={`${HongoStyle.clearDigital_Filter_search} relative w-full bg-transparent border-white border-[2px] border-solid text-[16px] font-medium font-poppins py-[21px] pl-[15px] pr-[40px] text-white outline-none `}
+                  value={searchValue}
+                  onChange={(e) => {
+                    setSearchValue(e.target.value);
+                    if (e.target.value === "") {
+                      showAllCards();
+                    }
+                  }}
                 />
                 <label htmlFor="fSearch" className=" text-[0] hidden ">
                   .
                 </label>
+                <div className="search_icon absolute top-[22px] right-[22px] w-[25px] h-[25px] ">
+                  <Image
+                    src="/clearDigital/arrow_search.svg"
+                    width={50}
+                    height={50}
+                    alt="search"
+                  />
+                </div>
+                {/* <div className="close_icon absolute top-[23px] right-[22px] w-[20px] h-[20px] hidden ">
+                  <Image
+                    src="/clearDigital/icon-close-white.svg"
+                    width={50}
+                    height={50}
+                    alt="close"
+                  />
+                </div> */}
               </div>
               <h5 className=" text-white text-[20px] font-bold font-poppins leading-[24px] mb-8 ">
                 Filter all insights
@@ -103,119 +170,6 @@ export default function ClearDigitalFilter() {
                   Clear all
                 </span>
               </div>
-              {/* <ul className=" filter_list_item relative w-full list-none ">
-                <li className=" relative w-full border-t-[1px] border-t-[#989898] border-solid pt-[30px] px-10 mb-[30px] ">
-                  <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] relative w-full inline-block  after:content-[''] after:absolute after:top-[0] after:right-[0] after:w-[24px] after:h-[24px] after:bg-[url('/clearDigital/arrow-up.svg')] after:bg-no-repeat after:bg-contain after:rotate-180 ">
-                    By Topic
-                  </span>
-                  <ul className=" relative w-full list-none mt-5 ">
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Brand Strategy
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Content Strategy
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Digital Marketing Process
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Digital Strategy
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Innovation & Trends
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Web Development
-                      </span>
-                    </li>
-                  </ul>
-                </li>
-                <li className=" relative w-full border-t-[1px] border-t-[#989898] border-solid pt-[30px] px-10 mb-[30px] ">
-                  <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] relative w-full inline-block  after:content-[''] after:absolute after:top-[0] after:right-[0] after:w-[24px] after:h-[24px] after:bg-[url('/clearDigital/arrow-up.svg')] after:bg-no-repeat after:bg-contain after:rotate-180 ">
-                    By Solution
-                  </span>
-                  <ul className=" relative w-full list-none mt-5 ">
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Brand Strategy
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Content Strategy
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Digital Marketing Process
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Digital Strategy
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Innovation & Trends
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Web Development
-                      </span>
-                    </li>
-                  </ul>
-                </li>
-                <li className=" relative w-full border-t-[1px] border-t-[#989898] border-solid pt-[30px] px-10 mb-[30px] ">
-                  <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] relative w-full inline-block  after:content-[''] after:absolute after:top-[0] after:right-[0] after:w-[24px] after:h-[24px] after:bg-[url('/clearDigital/arrow-up.svg')] after:bg-no-repeat after:bg-contain after:rotate-180 ">
-                    By Content Type
-                  </span>
-                  <ul className=" relative w-full list-none mt-5 ">
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Brand Strategy
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Content Strategy
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Digital Marketing Process
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Digital Strategy
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Innovation & Trends
-                      </span>
-                    </li>
-                    <li className=" relative w-full mb-4 ">
-                      <span className=" text-white text-[16px] font-bold font-poppins leading-[20px] ">
-                        Web Development
-                      </span>
-                    </li>
-                  </ul>
-                </li>
-              </ul> */}
               <ul className=" filter_list_item relative w-full list-none ">
                 <li className=" relative w-full border-t-[1px] border-t-[#989898] border-solid pt-[30px] px-10 mb-[30px] ">
                   <span
@@ -372,32 +326,34 @@ export default function ClearDigitalFilter() {
               <div className="card_wrapper relative w-mainRow ml-[-15px] flex flex-wrap ">
                 {filtercardData.map((filterCard) => (
                   <div
-                    className="filter_card group relative w-halfWidth mx-[15px] mb-[30px] min-h-[552px] flex flex-wrap items-end "
+                    className="filter_card group relative w-halfWidth mx-[15px] mb-[30px] "
                     key={filterCard.id}
                   >
-                    <Link
-                      href="/"
-                      className={`${HongoStyle.redirect_link} z-10`}
-                    >
-                      .
-                    </Link>
-                    <div className="img_wrap absolute top-0 left-0 w-full h-full ">
-                      <Image
-                        src={filterCard.imgUrl}
-                        width={1000}
-                        height={1000}
-                        alt="card_img"
-                        className=" w-full h-full object-cover "
-                      />
-                    </div>
-                    <div className="card_content relative p-[50px] bg-[#00000080] z-[1] ">
-                      <h3 className=" text-white text-[30px] font-bold font-poppins leading-[39px] overflow-ellipsis line-clamp-3 transition-all duration-500 ease-in-out group-hover:overflow-visible group-hover:line-clamp-none ">
-                        {filterCard.cardTitle}
-                      </h3>
-                      <div className="text relative w-full mt-5 h-full max-h-0 transition-all duration-500 ease-in-out  overflow-hidden group-hover:max-h-[1000px] group-hover:will-change-auto ">
-                        <p className=" text-white text-[16px] font-normal font-poppins leading-[24px] opacity-0 group-hover:opacity-[1] transition-opacity duration-500 ease-in-out ">
-                          {filterCard.cardParagraph}
-                        </p>
+                    <div className="wrapper relative w-full min-h-[552px] flex flex-wrap items-end ">
+                      <Link
+                        href="/"
+                        className={`${HongoStyle.redirect_link} z-10`}
+                      >
+                        .
+                      </Link>
+                      <div className="img_wrap absolute top-0 left-0 w-full h-full ">
+                        <Image
+                          src={filterCard.imgUrl}
+                          width={1000}
+                          height={1000}
+                          alt="card_img"
+                          className=" w-full h-full object-cover "
+                        />
+                      </div>
+                      <div className="card_content relative p-[50px] bg-[#00000080] z-[1] ">
+                        <h3 className=" text-white text-[30px] font-bold font-poppins leading-[39px] overflow-ellipsis line-clamp-3 transition-all duration-500 ease-in-out group-hover:overflow-visible group-hover:line-clamp-none ">
+                          {filterCard.cardTitle}
+                        </h3>
+                        <div className="text relative w-full mt-5 h-full max-h-0 transition-all duration-500 ease-in-out  overflow-hidden group-hover:max-h-[1000px] group-hover:will-change-auto ">
+                          <p className=" text-white text-[16px] font-normal font-poppins leading-[24px] opacity-0 group-hover:opacity-[1] transition-opacity duration-500 ease-in-out ">
+                            {filterCard.cardParagraph}
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
